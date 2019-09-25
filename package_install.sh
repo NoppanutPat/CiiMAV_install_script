cbeb319a03df8b93959e8642d0f6799586d441d9
Noppanut Thongton <noppanut.t@ku.th>
2019-09-25 12:30:23 +0000

    Add package_install.sh

---
+++ package_install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install python-pip
+sudo apt-get install vim
+
+pip install numpy
+pip install pandas
+pip install matplotlib
+pip install dronekit
+pip install dronekit-sitl
